86c0445fd6523050f0b83cc441d31bc18a191f86
Mattison Greff <greff.matt@icloud.com>
2018-06-30 19:34:35 -0700

    start to fix api server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-const express = require('express');
-
+const express    = require('express');
+const knexConfig = require("./knexfile");
+const knex       = require("knex")(knexConfig[ENV]);
 
 const PORT = process.env.API_PORT | 8081;       // this port needs to match the port in the webapack.config.js -> devServer -> proxy
 
