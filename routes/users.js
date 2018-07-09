d3ee6eef997d1b260753e8beef0ca2600732fdf4
Mattison Greff <greff.matt@icloud.com>
2018-07-08 21:49:54 -0700

    add route for all reports for an individual user

--- routes/users.js
+++ routes/users.js
@@ -27,6 +27,43 @@ module.exports = (knex) => {
       })
       
   });
+
+  router.get("/:id/reports", (req, res, next) => {
+    knex('reports')
+    .select(
+      'users.first_name',
+      'users.last_name',
+      'users.phone_number',
+      'users.policy_number',
+      'users.license_number',
+      'users.id as userid',
+      'vehicles.make',
+      'vehicles.model',
+      'vehicles.year',
+      'vehicles.damage',
+      'vehicles.id as vehicleid',
+      'description',
+      'reports.id',
+      'reports.created_at',
+      'reports.status',
+      'witnesses.first_name as witnesses_first_name',
+      'witnesses.last_name as witnesses_last_name',
+      'witnesses.testimony as witnesses_testimony',
+      'witnesses.id as witnessesid',
+      'media.uri',
+      'media.id as mediaid',
+    )
+    .where('user_id', '=', req.params.id)
+    .leftJoin('vehicles', 'reports.vehicle_id', '=', 'vehicles.id')
+    .leftJoin('witnesses', 'witnesses.report_id', 'reports.id')
+    .leftJoin('media', 'media.report_id', 'reports.id')
+    .then((results) => {
+      res.json(results)
+    }).catch((err) => {
+      res.json(err)
+    })
+  });
+
   router.get("/:id/vehicles", (req, res, next) => {
     knex.select('*').from('vehicles')
       .where('user_id', '=', req.params.id)
