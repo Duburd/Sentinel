c133195e57b5b53397cf43eee09737729f350c94
Mattison Greff <greff.matt@icloud.com>
2018-07-09 12:12:24 -0700

    fix and refactor routes for an individual user

--- routes/users.js
+++ routes/users.js
@@ -13,7 +13,6 @@ module.exports = (knex) => {
       .catch((e) => {
         res.status(500).send(e);
       })
-      
   });
 
   router.get("/:id", (req, res, next) => {
@@ -29,34 +28,23 @@ module.exports = (knex) => {
   });
 
   router.get("/:id/reports", (req, res, next) => {
+    console.log(req.params.id)
     knex('reports')
     .select(
-      'users.first_name',
-      'users.last_name',
-      'users.phone_number',
-      'users.policy_number',
-      'users.license_number',
-      'users.id as userid',
       'vehicles.make',
       'vehicles.model',
       'vehicles.year',
+      'vehicles.plate',
       'vehicles.damage',
       'vehicles.id as vehicleid',
       'description',
       'reports.id',
       'reports.created_at',
       'reports.status',
-      'witnesses.first_name as witnesses_first_name',
-      'witnesses.last_name as witnesses_last_name',
-      'witnesses.testimony as witnesses_testimony',
-      'witnesses.id as witnessesid',
-      'media.uri',
-      'media.id as mediaid',
     )
-    .where('user_id', '=', req.params.id)
+    .from('reports')
+    .where('reports.user_id', req.params.id)
     .leftJoin('vehicles', 'reports.vehicle_id', '=', 'vehicles.id')
-    .leftJoin('witnesses', 'witnesses.report_id', 'reports.id')
-    .leftJoin('media', 'media.report_id', 'reports.id')
     .then((results) => {
       res.json(results)
     }).catch((err) => {
